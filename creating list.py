b6e4189488f7040874c9f79a87310e02d5043c16
Mallikarjun-s-biradar <115518966+Mallikarjun-s-biradar@users.noreply.github.com>
2022-10-11 11:19:54 +0530

    Add files via upload

---
+++ creating list.py
@@ -0,0 +1,14 @@
+m=list()
+print (m)
+#creating list from strings
+l1=list ('hello')
+print (l1)
+#creating list from tuple
+t=('q','w','e','r','t','y',2)
+n=list(t)
+print(n)
+l2=list(input("enter list numbers :"))
+print(l2)
+list=eval(input("enter list to be added :"))
+print (list)
+      
